57b0ec8fdf2474abd888aefb9aeff964cb697303
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-05-20 17:57:25 +0200

    use file content instead of file_path

--- modules/read/outputs.tf
+++ modules/read/outputs.tf
@@ -1,4 +1,4 @@
 output "output_with_prefix" {
   description = "Output from the 'files' module with a prefix"
-  value       = "${var.prefix} ${module.files.file_paths}"
+  value       = "${var.prefix} ${module.files.file_content}"
 }
